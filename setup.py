cd3a29f265c578b8396203f03698ca36b90594b4
SneznyKocur <kocursnezny@gmail.com>
2023-02-10 16:22:54 +0100

    update version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 import os
-VERSION = '0.0.18'
+VERSION = '0.1.0'
 DESCRIPTION = 'Wrapper for fbchat'
 LONG_DESCRIPTION = 'Simple Wrapper Package to make Programming with fbchat easier and simpler to read.'
 
