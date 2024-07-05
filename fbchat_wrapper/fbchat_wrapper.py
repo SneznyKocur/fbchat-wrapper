6075d3c7c4d6866f3f27dda295869b05f9e426b5
SneznyKocur <sneznykocur@sneznykocur.lol>
2024-07-05 18:53:34 +0200

    bugfix

--- fbchat_wrapper/fbchat_wrapper.py
+++ fbchat_wrapper/fbchat_wrapper.py
@@ -16,7 +16,7 @@ import py_fbchat as fbchat
 from py_fbchat.models import Message, ThreadType
 from PIL import Image
 from PIL import ImageDraw
-
+import logging
 from PIL import ImageFont
 import ffmpeg
 from zipfile import ZipFile
@@ -42,12 +42,12 @@ class Wrapper(fbchat.Client):
     includes most functions
     """
     def __init__(self, email: str, password: str, prefix=""):
-        
+        self.logger = logging.getLogger("wrapper")
         _setup()
         self._command_list = dict()
         self._event_list = dict()
         self.Prefix = prefix or "!"
-        print("logging in")
+        self.logger.info("logging in")
         super().__init__(email, password)
 
     def _addEvent(self,name,func):
@@ -99,35 +99,47 @@ class Wrapper(fbchat.Client):
     def onMessage(
         self, author_id, message_object, thread_id, thread_type, ts, **kwargs
     ):
-        """DO NOT CALL (event handle)"""
+        
+        """DO NOT CALL"""
         print("got message")
+        self.logger.debug("got message")
         if message_object.author == self.uid:
+            print(message_object.author,"is",self.uid)
             return
         self.mid = message_object.uid
         try:
-            self.markAsDelivered(thread_id, message_object.uid)
-            self.markAsRead(thread_id)
+            #self.markAsDelivered(thread_id, message_object.uid)
+            #self.markAsRead(thread_id)
+            ...
         except:
-            print("Failed to mark as read")
+            self.logger.warning("Failed to mark as read")
         self.thread = (thread_id, thread_type)
+        print(self.thread)
         self.text = message_object.text
-        self.author = self.utils_getUserName(author_id)
-
-        if not self.text: return
-
-        if not self.text.startswith(self.Prefix):
+        print(self.text)
+        #self.author = self.utils.getUserName(author_id)
+        self.author = self.fetchUserInfo(author_id)[author_id].name
+        print(self.author,self.text)
+        if not self.text: 
+            print("no text")
             if "onMessage" in self._event_list:
                 t = threading.Thread(target=self._event_list["onMessage"],kwargs={"author_id":author_id,"message_object":message_object,"thread_id":thread_id,"thread_type":thread_type,"ts":ts})
                 t.start()
             return
 
+        if not self.text.startswith(self.Prefix):
+            print(self.text,"doesn't start with",self.Prefix) 
+            if "onMessage" in self._event_list:
+                t = threading.Thread(target=self._event_list["onMessage"],kwargs={"author_id":author_id,"message_object":message_object,"thread_id":thread_id,"thread_type":thread_type,"ts":ts})
+                t.start()
+            return
         commandName = self.text.replace(self.Prefix, "", 1).split(" ")[0]
         args = list()
         _args = self.text.replace(self.Prefix, "", 1).replace(commandName, "", 1)
         parts = self._arg_split(_args)
         for part in parts:
             args.append(part)
-
+        print(commandName,args)
         if not commandName in self._command_list:
             self.reply(f"{commandName} is an invalid command")
             raise CommandNotRegisteredException
@@ -137,7 +149,7 @@ class Wrapper(fbchat.Client):
         argsdict = dict()
         for i, x in enumerate(self._command_list[commandName][1]):
             argsdict.update({x: args[i]})
-        print(f"calling command {command} in {self.thread[0]}")
+        self.logger.info(f"calling command {command} in {self.thread[0]}")
         t = threading.Thread(
             target=command,
             kwargs={
@@ -420,66 +432,8 @@ class Wrapper(fbchat.Client):
         else:
             self.sendLocalFiles(filepath, message=message, thread_id=thread_id, thread_type=thread_type)
 
-    def utils_isURL(self, input):
-        """Check if input is url
 
-        Args:
-            input (str): input
-
-        Returns:
-            bool: is url?
-        """
-        return validators.url(input)
-    def utils_compressVideo(self, input, output):
-        """Compresses video to be sendable with messenger
-
-        Args:
-            input (str): input video file path
-            output (str): output video file path
-        """
-        # Reference: https://en.wikipedia.org/wiki/Bit_rate#Encoding_bit_rate
-        min_audio_bitrate = 32000
-        max_audio_bitrate = 256000
-
-        probe = ffmpeg.probe(input)
-        # Video duration, in s.
-        duration = float(probe["format"]["duration"])
-        # Audio bitrate, in bps.
-        audio_bitrate = float(
-            next((s for s in probe["streams"] if s["codec_type"] == "audio"), None)[
-                "bit_rate"
-            ]
-        )
-        # Target total bitrate, in bps.
-        target_total_bitrate = (50000 * 1024 * 8) / (1.073741824 * duration)
-
-        # Target audio bitrate, in bps
-        if 10 * audio_bitrate > target_total_bitrate:
-            audio_bitrate = target_total_bitrate / 10
-            if audio_bitrate < min_audio_bitrate < target_total_bitrate:
-                audio_bitrate = min_audio_bitrate
-            elif audio_bitrate > max_audio_bitrate:
-                audio_bitrate = max_audio_bitrate
-        # Target video bitrate, in bps.
-        video_bitrate = target_total_bitrate - audio_bitrate
-
-        i = ffmpeg.input(input)
-        ffmpeg.output(
-            i,
-            os.devnull,
-            **{"c:v": "libx264", "b:v": video_bitrate, "pass": 1, "f": "mp4"},
-        ).overwrite_output().run()
-        ffmpeg.output(
-            i,
-            output,
-            **{
-                "c:v": "libx264",
-                "b:v": video_bitrate,
-                "pass": 2,
-                "c:a": "aac",
-                "b:a": audio_bitrate,
-            },
-        ).overwrite_output().run()
+    
     def utils_threadCount(self) -> int:
         """get current alive thread count
 
@@ -487,116 +441,5 @@ class Wrapper(fbchat.Client):
             int: current alive thread count
         """
         return len(threading.enumerate())
-    def utils_genHelpImg(self,footer:str = None) -> str:
-        """Generates help image from commands
-
-        Args:
-            footer (str, optional): _image footer. Defaults to None.
-
-        Returns:
-            str: file path
-        """
-        helpdict = dict()
-        for x in self._command_list:
-            helpdict.update(
-                {
-                    x: {
-                        "description": self._command_list[x][2],
-                        "args": self._command_list[x][1],
-                    }
-                }
-            )
-        # desciption = 2
-        # args = 1
-        # func = 0
-
-        img = Image.new("RGBA", (300, 300), color=(20, 20, 20))
-        I1 = ImageDraw.Draw(img)
-        font = ImageFont.truetype("./font.ttf")
-
-        for i, name in enumerate(helpdict):
-
-            I1.text((0, (i + 1) * 10), name, (255, 255, 255), font) # name
-        
-            for y, x in enumerate(helpdict[name]["args"]):
-                I1.text(((5+7*y) * 10, (i + 1) * 10), x, (255, 255, 0), font) # args
-
-            I1.text(
-                (4 * 20 + 100, (i + 1) * 10),
-                helpdict[name]["description"],
-                (255, 255, 255),
-                font,
-            )
-
-        I1.text((0, 290), footer, (190, 255, 190), font)
-        img.save("./help.png")
-        return os.path.abspath("./help.png")
-
-    def utils_searchForUsers(self,query: str) -> list:
-        """Searches for users
-
-        Args:
-            query (str): query to search for
-
-        Returns:
-            list: list of user ids
-        """
-        _ = []
-        for user in self.searchForUsers(query):
-            _.append(user.uid)
-        return _
-
-    def utils_getIDFromUserIndex(self, userindex:str) -> int:
-        """Fetches id of user @ userindex
-
-        Args:
-            userindex (str): username[index]
-
-        Returns:
-            int: user.uid
-        """
-        name = userindex.split("[")[0]
-        ids = self.searchForUsers(name)
-        return ids[int(userindex.split("[")[1].replace("]",""))]
     
-    def utils_getUserName(self, id: int):
-        """Gets the username of user @ id
-
-        Args:
-            id (int): id of user
-        Returns:
-            str: username
-        """
-        return self.fetchUserInfo(id)[id].name
     
-    def utils_getThreadType(self,thread_id: int) -> ThreadType:
-        """Gets threadtype of a thread @ thread_id
-
-        Args:
-            thread_id (int): the id
-
-        Returns:
-            ThreadType: type of thread
-        """
-        return self.fetchThreadInfo(thread_id)[thread_id].type
-    def utils_getThreadFromUserIndex(self,userindex: str) -> tuple:
-        """Fetches thread from user @ userindex
-
-        Args:
-            userindex (str): username[index]
-
-        Returns:
-            tuple: thread tuple
-        """
-        if not userindex: return
-        if userindex.isnumeric(): 
-            thread_type = self.getThreadType(int(userindex))
-            thread_id = userindex
-            thread = (thread_id,thread_type)
-        else:
-            name = userindex.split("[")[0]
-            ids = self.searchForUsers(name)
-            thread_id = ids[int(userindex.split("[")[1].replace("]",""))]
-            thread_type = self.getThreadType(int(thread_id)) 
-            thread = (thread_id,thread_type)
-        return thread
